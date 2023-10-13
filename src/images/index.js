850fa7a58ae4ec481ee6bedfc7b961f64e444084
nurmarief <ariefmuhammad897@gmail.com>
2023-10-13 20:18:54 +0700

    create images dir

---
+++ src/images/index.js
@@ -0,0 +1,5 @@
+import Favicon from './favicon-32x32.png';
+import IconStar from './icon-star.svg';
+import IllustrationThankYou from './illustration-thank-you.svg';
+
+export { Favicon, IconStar, IllustrationThankYou };
